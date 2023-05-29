e95e94d86e6487b72b982a596b3094493cb6c11f
MariyaParus <cat-she@mail.ru>
2023-05-29 15:53:29 +0300

    add docstrings

--- src/functions.py
+++ src/functions.py
@@ -7,13 +7,23 @@ current_path = os.path.dirname(os.path.realpath(__file__))
 operations_file_path = os.path.join(current_path, '..', 'operations.json')
 
 
-def read_file(file_path=operations_file_path):
+def read_file(file_path: str = operations_file_path) -> list:
+    """
+    Читает файл, возвращает данные
+    :param file_path: путь к открываемому файлу
+    :return: список словарей с данными
+    """
     with open(file_path, 'r') as file:
         data = json.load(file)
         return data
 
 
-def show_last_operations(data):
+def show_last_operations(data: list) -> str:
+    """
+    Показывает последние операции
+    :param data: список словарей с данными
+    :return: строка с результатами
+    """
     last_operations = [op for op in data if op.get("state") == "EXECUTED"]
     recent_operations = sorted(last_operations, key=lambda op: op["date"], reverse=True)[:5]
 
@@ -27,22 +37,29 @@ def show_last_operations(data):
         op_to = mask_account(op['to'])
 
         result.append(f'{op_date} {op_description}\n{op_from} -> {op_to}\n{op_amount} {op_currency}\n')
-        print(result)
     return "\n".join(result)
 
 
-def mask_card(card_number):
+def mask_card(card_number: str) -> str:
+    """
+    Маскирует номер карты или счета откуда пришел перевод
+    :param card_number: номер карты или счета
+    :return: маскированный номер карты или счета
+    """
     if card_number:
         card_name = ' '.join(card_number.split()[:-1])
         c_num = card_number.split()[-1]
         formatted_account = f"{c_num[:4]} {c_num[4:6]}{c_num[6:12].replace(c_num[6:12], '** ****')} {c_num[-4:]}"
         return f'{card_name} {formatted_account}'
-    return None
 
 
-def mask_account(account_number):
+def mask_account(account_number: str) -> str:
+    """
+    Маскирует номер карты или счета на который пришел перевод
+    :param account_number: номер карты или счета
+    :return: маскированный номер карты или счета
+    """
     if account_number:
         acc_name = ' '.join(account_number.split()[:-1])
         a_num = account_number.split()[-1]
         return f'{acc_name} **{a_num[-4:]}'
-    return None
